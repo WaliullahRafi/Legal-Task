f4a1d8451119dd11067e55f361c28a1b284822e1
WaliullahRafi <rafiwaliullah0@gamil.com>
2024-01-21 16:43:32 +0600

    made with Fokira Tailwind & Daisy Ui

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+
+
+  module.exports = {
+    theme: {
+      extend: {
+        backgroundImage: {
+          'hero-pattern': "url('assets/banner.png)"
+        }
+      }
+    }
+  }
